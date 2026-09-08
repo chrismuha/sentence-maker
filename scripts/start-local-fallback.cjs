7a3bed4c1b977e102b4237bf748d31cccf94fa91
Chris Muha <muhachris297@gmail.com>
2026-09-08 10:24:42 -0400

    Enforce assigned development port

--- scripts/start-local-fallback.cjs
+++ scripts/start-local-fallback.cjs
@@ -167,7 +167,7 @@ async function main() {
   const viteBin = binPath('vite');
   const electronBin = binPath('electron');
 
-  const renderer = spawnProcess(viteBin, ['dev', '--host', '127.0.0.1', '--port', String(freePort)], env);
+  const renderer = spawnProcess(viteBin, ['dev', '--host', '127.0.0.1', '--port', String(freePort), '--strictPort'], env);
   await waitForServer(freePort);
   const main = spawnProcess(electronBin, ['.', '--disable-cache', '--disable-gpu', '--disable-gpu-compositing', '--disable-logging', '--log-level=3'], env);
 
